00680563455f808dc9a352fd23b204163aa8278b
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-30 16:58:42 +0300

    add parser

---
+++ src/main/kotlin/data/csv/parser/TaskCsvParser.kt
@@ -0,0 +1,38 @@
+package org.qudus.squad.data.csv.parser
+
+import kotlinx.datetime.toLocalDateTime
+import org.qudus.squad.data.csv.CsvParser
+import org.qudus.squad.model.State
+import org.qudus.squad.model.Task
+
+class TaskCsvParser : CsvParser<Task> {
+
+    override fun fromCsvRow(row: String): Task {
+        val cleanedLine = row.trim().removeSuffix("\n")
+        val taskList = cleanedLine.split(',')
+        return Task(
+            id = taskList[TaskCsvColumnIndex.ID.index],
+            title = taskList[TaskCsvColumnIndex.TITLE.index],
+            description = taskList[TaskCsvColumnIndex.DESCRIPTION.index],
+            projectId = taskList[TaskCsvColumnIndex.PROJECT_ID.index],
+            state = State(taskList[TaskCsvColumnIndex.STATE_ID.index], taskList[TaskCsvColumnIndex.STATE_NAME.index]),
+            creatorUserID = taskList[TaskCsvColumnIndex.CREATOR_USER_ID.index],
+            createdAt = taskList[TaskCsvColumnIndex.CREATED_AT.index].toLocalDateTime(),
+            lastUpdatedAt = taskList[TaskCsvColumnIndex.LAST_UPDATED_AT.index].toLocalDateTime(),
+        )
+    }
+
+    override fun toCsvRow(model: Task): String {
+        return listOf(
+           model.id,
+            model.title,
+            model.description,
+            model.projectId,
+            model.state.id,
+            model.state.name,
+            model.creatorUserID,
+            model.createdAt.toString(),
+            model.lastUpdatedAt.toString()
+        ).joinToString(",")
+    }
+}
