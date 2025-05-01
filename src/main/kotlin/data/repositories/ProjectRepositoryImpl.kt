643edad6c2462531d912770334c23b46acdf744e
ahmed-madhoun1 <ahmed2madhoun2@gmail.com>
2025-05-01 14:00:49 +0300

    Create delete project and get all project use cases with its implementation in ProjectRepository

--- src/main/kotlin/data/repositories/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/ProjectRepositoryImpl.kt
@@ -1,6 +1,17 @@
 package org.qudus.squad.data.repositories
 
+import org.qudus.squad.data.data_source.project_data_source.ProjectDataSource
 import org.qudus.squad.logic.repositories.ProjectRepository
+import org.qudus.squad.model.Project
 
-class ProjectRepositoryImpl: ProjectRepository {
+class ProjectRepositoryImpl(private val projectDataSource: ProjectDataSource) : ProjectRepository {
+
+    override fun getAllProjects(): List<Project> {
+        return projectDataSource.getAllProjects()
+    }
+
+    override fun deleteProjectById(id: String) {
+        return projectDataSource.deleteProjectById(id)
+    }
+
 }
--- src/main/kotlin/logic/repositories/ProjectRepository.kt
+++ src/main/kotlin/logic/repositories/ProjectRepository.kt
@@ -1,4 +1,8 @@
 package org.qudus.squad.logic.repositories
 
+import org.qudus.squad.model.Project
+
 interface ProjectRepository {
+    fun getAllProjects(): List<Project>
+    fun deleteProjectById(id: String)
 }
