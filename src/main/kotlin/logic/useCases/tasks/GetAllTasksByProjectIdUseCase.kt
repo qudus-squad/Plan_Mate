8674a88507a3d300686d18a73579e4bfccd74565
Mahmoud A. El Yamany <mahmoud.elyamany.official@gmail.com>
2025-05-02 17:01:04 +0300

    add feature GetAllTasksByProjectIdUseCase

---
+++ src/main/kotlin/logic/useCases/tasks/GetAllTasksByProjectIdUseCase.kt
@@ -0,0 +1,18 @@
+package org.qudus.squad.logic.useCases.tasks
+
+import org.qudus.squad.logic.repositories.TaskRepository
+import org.qudus.squad.model.entity.Task
+import org.qudus.squad.model.exceptions.NoTasksFoundException
+
+class GetAllTasksByProjectIdUseCase(
+    private val taskRepository: TaskRepository
+) {
+
+    fun getAllTasksByProjectId(id: String) : List<Task>? {
+        return taskRepository.getAllTasksByProjectId(id).takeIf { it.isNotEmpty() } ?: throw NoTasksFoundException(NO_TASK_FOUND)
+    }
+
+    companion object {
+        const val NO_TASK_FOUND = "No tasks found"
+    }
+}
