c69f4e440244f34f5def40e2e5c5885c2a6edef1
Farah315 <farah.53kh1@hotmail.com>
2025-05-02 14:56:59 +0300

    add use case to get and save logs

--- src/main/kotlin/logic/useCases/log/SaveLogUseCase.kt
+++ src/main/kotlin/logic/useCases/log/SaveLogUseCase.kt
@@ -1,4 +1,10 @@
 package org.qudus.squad.logic.useCases.log
 
-class SaveLogUseCase {
+import org.qudus.squad.logic.repositories.LogRepository
+import org.qudus.squad.model.entity.LogEntry
+
+class SaveLogUseCase(private val logRepository: LogRepository){
+    fun saveLog(logEntry: LogEntry) {
+        logRepository.addLog(logEntry)
+    }
 }
