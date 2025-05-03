2aeec563185629f4e25b389810d91ada7608b8bc
Talaat <Abdo.talat740@gmil.com>
2025-05-03 07:29:14 +0300

    get all users use case

--- src/test/kotlin/logic/useCases/user/AddNewUserUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/user/AddNewUserUseCaseTest.kt
@@ -2,18 +2,15 @@ package logic.useCases.user
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.qudus.squad.logic.repositories.UserRepository
 import org.qudus.squad.logic.useCases.user.AddNewUserUseCase
 import org.qudus.squad.logic.utils.EncryptionByUsingMD5
 import org.qudus.squad.logic.validation.UserDataValidator
-import org.qudus.squad.model.entity.User
 import org.qudus.squad.model.entity.UserRole
 import org.qudus.squad.model.exceptions.AccessDeniedException
 import org.qudus.squad.model.exceptions.InvalidUserDataException
-import org.qudus.squad.model.exceptions.UserAlreadyExistException
 import kotlin.test.Test
 
 class AddNewUserUseCaseTest {
@@ -109,30 +106,4 @@ class AddNewUserUseCaseTest {
         }
 
     }
-
-    @Test
-    fun `should throw UserAlreadyExistException when the user already exists`() {
-        // Given
-        val currentUserRole = UserRole.ADMIN
-        val username = "Abdo"
-        val password = "12356"
-        val userRole = UserRole.MATE
-
-        val user = User(username = username, passwordHash = mD5.generateHash(password), role = userRole)
-
-        every {
-            userRepository.addNewUser(user)
-        } throws UserAlreadyExistException("User Already Exist")
-
-        // When & Then
-        shouldThrow<UserAlreadyExistException> {
-            addNewUserUseCase.addUser(
-                currentUserRole = currentUserRole,
-                username = username,
-                password = password,
-                userRole = userRole
-            )
-        }
-    }
-
 }
