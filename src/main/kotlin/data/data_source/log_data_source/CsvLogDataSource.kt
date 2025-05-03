8a241f1d5a09d022daef8e62a5516709454ddbdd
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-05-03 04:33:56 +0300

    change read write to file

--- src/main/kotlin/data/data_source/log_data_source/CsvLogDataSource.kt
+++ src/main/kotlin/data/data_source/log_data_source/CsvLogDataSource.kt
@@ -1,47 +1,44 @@
 package org.qudus.squad.data.data_source.log_data_source
 
-import okio.FileSystem
-import okio.Path.Companion.toPath
-import okio.buffer
 import org.qudus.squad.data.csv.CsvReader
 import org.qudus.squad.data.csv.parser.LogEntryCsvParser
-import org.qudus.squad.data.data_source.project_data_source.CsvProjectDataSource.Companion.PROJECTS_FILE
+import org.qudus.squad.data.data_source.WriteInFileUseCase
 import org.qudus.squad.model.entity.LogEntry
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.appendText
 
 class CsvLogDataSource(
-    private val csvReader: CsvReader, private val logEntryCsvParser: LogEntryCsvParser
+    private val csvReader: CsvReader,
+    private val logEntryCsvParser: LogEntryCsvParser,
+    private val writeInFileUseCase: WriteInFileUseCase
 ) : LogDataSource {
 
+
     override fun addLog(logEntry: LogEntry) {
-        val csvLine = logEntryCsvParser.toCsvRow(logEntry)
-        FileSystem.SYSTEM.appendingSink(PROJECTS_FILE.toPath()).buffer().use { sink ->
-            sink.writeUtf8(csvLine + "\n")
-        }
+        val csvLine = logEntryCsvParser.toCsvRow(logEntry) + "\n"
+        writeInFileUseCase.writeLineToFile(LOGS_FILE,csvLine)
     }
 
     override fun getLogByTargetId(targetId: String): List<LogEntry>? {
-        val filteredLogs = getAllLogs().filter { log -> log.targetId == targetId }
+        val filteredLogs = getAllLogs().filter { it.targetId == targetId }
         return filteredLogs.ifEmpty { null }
     }
 
     override fun getAllLogs(): List<LogEntry> {
-        return csvReader.read(LOGS_FILE).mapNotNull { line ->
+        return csvReader.read(LOGS_FILE).mapNotNull {
             try {
-                logEntryCsvParser.fromCsvRow(line)
-            } catch (e: IllegalArgumentException) {
+                logEntryCsvParser.fromCsvRow(it)
+            } catch (_: IllegalArgumentException) {
                 null
             }
         }
     }
 
     override fun deleteLogByTargetId(targetId: String) {
-        val logs = getAllLogs().filterNot { it.targetId == targetId }
-        FileSystem.SYSTEM.sink(PROJECTS_FILE.toPath()).buffer().use { sink ->
-            logs.forEach { log ->
-                val csvLine = logEntryCsvParser.toCsvRow(log)
-                sink.writeUtf8(csvLine + "\n")
-            }
-        }
+        val filteredLogs = getAllLogs().filterNot { it.targetId == targetId }
+        val csvLines = filteredLogs.map{log -> logEntryCsvParser.toCsvRow(log)}
+        writeInFileUseCase.writeLinesToFile(LOGS_FILE, csvLines)
     }
 
     companion object {
