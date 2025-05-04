82babd257640bffb246c0b73668ed370ec3b9754
fara7 <fara734isgreat@gmail.com>
2025-05-04 14:54:13 +0300

    remove the nullability

--- src/main/kotlin/logic/repositories/LogRepository.kt
+++ src/main/kotlin/logic/repositories/LogRepository.kt
@@ -4,7 +4,7 @@ import org.qudus.squad.model.entity.LogEntry
 
 interface LogRepository {
     fun addLog(logEntry: LogEntry)
-    fun getLogByTargetId(targetId: String): List<LogEntry>?
-    fun getAllLogs(): List<LogEntry>?
+    fun getLogByTargetId(targetId: String): List<LogEntry>
+    fun getAllLogs(): List<LogEntry>
     fun deleteLogByTargetId(targetId: String)
 }
