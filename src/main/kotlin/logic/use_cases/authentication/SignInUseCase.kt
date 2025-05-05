c9edfd077e0863b7c0216a38acc4f49d5cf77f86
Talaat <Abdo.talat740@gmil.com>
2025-05-05 09:01:50 +0300

    make the user data validation use case more abstract

--- src/main/kotlin/logic/use_cases/authentication/SignInUseCase.kt
+++ src/main/kotlin/logic/use_cases/authentication/SignInUseCase.kt
@@ -1,7 +1,5 @@
 package logic.use_cases.authentication
 
-import org.qudus.squad.logic.exceptions.InvalidPasswordException
-import org.qudus.squad.logic.exceptions.InvalidUserNameException
 import org.qudus.squad.logic.repositories.AuthenticationRepository
 import org.qudus.squad.logic.validation.UserDataValidationUseCase
 import org.qudus.squad.model.entity.User
@@ -11,20 +9,7 @@ class SignInUseCase(
     private val userDataValidationUseCase: UserDataValidationUseCase
 ) {
     fun signIn(username: String, password: String): User {
-        if (!userDataValidationUseCase.isValidUserName(username)) {
-            throw InvalidUserNameException(INVALID_USER_NAME)
-        }
-        if (!userDataValidationUseCase.isValidPassword(password)) {
-            throw InvalidPasswordException(INVALID_PASSWORD)
-        }
+        userDataValidationUseCase.validateUserData(username,password)
         return authenticationRepository.signIn(username, password)
     }
-
-    companion object {
-        const val INVALID_USER_NAME =
-            "Invalid username, username shouldn't be empty or contains symbols but \'.\' or \'_\' "
-        const val INVALID_PASSWORD = "Password should be 8 characters or more"
-
-    }
-
 }
--- src/main/kotlin/logic/use_cases/user/AddNewUserUseCase.kt
+++ src/main/kotlin/logic/use_cases/user/AddNewUserUseCase.kt
@@ -1,8 +1,6 @@
 package logic.use_cases.user
 
 import org.qudus.squad.logic.exceptions.AccessDeniedException
-import org.qudus.squad.logic.exceptions.InvalidPasswordException
-import org.qudus.squad.logic.exceptions.InvalidUserNameException
 import org.qudus.squad.logic.repositories.UserRepository
 import org.qudus.squad.logic.utils.EncryptionByUsingMD5
 import org.qudus.squad.logic.validation.UserDataValidationUseCase
@@ -23,20 +21,12 @@ class AddNewUserUseCase(
         if (currentUserRole != UserRole.ADMIN) {
             throw AccessDeniedException(NOT_AUTHORIZED_EXCEPTION_MESSAGE)
         }
-        if (!userValidator.isValidUserName(username)) {
-            throw InvalidUserNameException(INVALID_USER_NAME)
-        }
-        if (!userValidator.isValidPassword(password)) {
-            throw InvalidPasswordException(INVALID_PASSWORD)
-        }
+        userValidator.validateUserData(username, password)
         val user = User(username = username, passwordHash = mD5.generateHash(password), role = userRole)
         return userRepository.addNewUser(user)
     }
 
     companion object {
         const val NOT_AUTHORIZED_EXCEPTION_MESSAGE = "User is not authorized to perform this action."
-        const val INVALID_USER_NAME =
-            "Invalid username, username shouldn't be empty or contains symbols but \'.\' or \'_\' "
-        const val INVALID_PASSWORD = "Password should be 8 characters or more"
     }
 }
