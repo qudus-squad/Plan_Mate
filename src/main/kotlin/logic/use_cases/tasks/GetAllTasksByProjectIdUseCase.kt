507124decf9520e4fa58144e0fccb4cd8c349e17
Mousaber00 <mousaber00@gmail.com>
2025-05-06 16:58:22 +0300

    resolve comment on git

--- src/main/kotlin/logic/use_cases/tasks/GetAllTasksByProjectIdUseCase.kt
+++ src/main/kotlin/logic/use_cases/tasks/GetAllTasksByProjectIdUseCase.kt
@@ -1,6 +1,5 @@
 package logic.use_cases.tasks
 
-import org.qudus.squad.logic.exceptions.TaskNotFoundException
 import org.qudus.squad.logic.repositories.TaskRepository
 import org.qudus.squad.model.entity.Task
 
@@ -9,8 +8,7 @@ class GetAllTasksByProjectIdUseCase(
 ) {
 
     fun getAllTasksByProjectId(projectId: String) : List<Task> {
-        return taskRepository.getAllTasksByProjectId(id)
-            .takeIf { it.isNotEmpty() } ?: emptyList()
+        return taskRepository.getAllTasksByProjectId(projectId)
     }
 
     companion object {
