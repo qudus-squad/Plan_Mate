34f668d35cef32229df3dbd07b18d9cb1f804bb4
fara7 <fara734isgreat@gmail.com>
2025-05-06 17:30:35 +0300

    add Get User By Id Use Case class

---
+++ src/main/kotlin/logic/use_cases/user/GetUserByIdUseCase.kt
@@ -0,0 +1,10 @@
+package org.qudus.squad.logic.use_cases.user
+
+import org.qudus.squad.logic.repositories.UserRepository
+import org.qudus.squad.model.entity.User
+
+class GetUserByIdUseCase(private val userRepository: UserRepository) {
+    fun getUserById(userid: String): User {
+        return userRepository.getUserById(userid)
+    }
+}
