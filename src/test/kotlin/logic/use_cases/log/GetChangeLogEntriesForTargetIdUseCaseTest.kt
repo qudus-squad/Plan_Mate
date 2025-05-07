9ff07c5fc3a4f2a8188e82ffd611d578e6f8e37d
fara7 <fara734isgreat@gmail.com>
2025-05-07 22:52:02 +0300

    add coEvery and runTest

--- src/test/kotlin/logic/use_cases/log/GetChangeLogEntriesForTargetIdUseCaseTest.kt
+++ src/test/kotlin/logic/use_cases/log/GetChangeLogEntriesForTargetIdUseCaseTest.kt
@@ -2,8 +2,9 @@ package logic.use_cases.log
 
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
-import io.mockk.every
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
@@ -54,42 +55,44 @@ class GetChangeLogEntriesForTargetIdUseCaseTest {
 
     @Test
     fun `should return change logs for target id when history exists`() {
-        // Given
-        val targetId = "xyz-123"
-        val logs = sampleLogEntries()
-        every { logRepository.getLogByTargetId(targetId) } returns logs
+        runTest {
+            // Given
+            val targetId = "xyz-123"
+            val logs = sampleLogEntries()
+            coEvery { logRepository.getLogByTargetId(targetId) } returns logs
 
-        // When
-        val result = getChangeLogEntriesForTargetIdUseCase.getFormattedLog(targetId)
+            // When
+            val result = getChangeLogEntriesForTargetIdUseCase.getFormattedLog(targetId)
 
-        // Then
-        result.shouldContainExactly(
-            listOf(
-                "user farah changed project xyz-123 from old name to new name at ${
-                    DateTimeFormatter.formatDateTimeForDisplay(
-                        logs[0].loggedAt
-                    )
-                }",
-                "user heba changed project xyz-123 from new name to final name at ${
-                    DateTimeFormatter.formatDateTimeForDisplay(
-                        logs[1].loggedAt
-                    )
-                }"
+            // Then
+            result.shouldContainExactly(
+                listOf(
+                    "user farah changed project xyz-123 from old name to new name at ${
+                        DateTimeFormatter.formatDateTimeForDisplay(
+                            logs[0].loggedAt
+                        )
+                    }",
+                    "user heba changed project xyz-123 from new name to final name at ${
+                        DateTimeFormatter.formatDateTimeForDisplay(
+                            logs[1].loggedAt
+                        )
+                    }"
+                )
             )
-        )
+        }
     }
 
     @Test
     fun `should return empty list when no Logs exist for target id`() {
-        // Given
-        val targetId = "123"
-        every { logRepository.getLogByTargetId(targetId) } returns emptyList()
+   runTest { // Given
+       val targetId = "123"
+       coEvery { logRepository.getLogByTargetId(targetId) } returns emptyList()
 
-        // When
-        val result = getChangeLogEntriesForTargetIdUseCase.getFormattedLog(targetId)
+       // When
+       val result = getChangeLogEntriesForTargetIdUseCase.getFormattedLog(targetId)
 
-        // Then
-        result shouldBe emptyList()
-    }
-
+       // Then
+       result shouldBe emptyList()
+   }
+  }
 }
