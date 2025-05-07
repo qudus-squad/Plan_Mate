a382d4450490e631f20c987e164844e78ebaec57
fara7 <fara734isgreat@gmail.com>
2025-05-07 22:52:42 +0300

    edit getLogByTargetId   suspend function

--- src/main/kotlin/data/data_source/log_data_source/remote/MongoLogDataSource.kt
+++ src/main/kotlin/data/data_source/log_data_source/remote/MongoLogDataSource.kt
@@ -1,6 +1,8 @@
 package org.qudus.squad.data.data_source.log_data_source.remote
 
+import com.mongodb.client.model.Filters
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import kotlinx.coroutines.flow.toList
 import org.qudus.squad.data.data_source.log_data_source.LogDataSource
 import org.qudus.squad.model.entity.LogEntry
 
@@ -11,10 +13,11 @@ class MongoLogDataSource(
         mongoDataBase.getCollection<LogEntryDto>("logs").insertOne(logEntry.toLogEntryDto())
     }
 
-    override fun getLogByTargetId(targetId: String): List<LogEntry> {
-        TODO("Not yet implemented")
+    override suspend fun getLogByTargetId(targetId: String): List<LogEntry> {
+        val dtoLogs= mongoDataBase.getCollection<LogEntryDto>("logs").find(Filters.eq("targetId"))
+        return  dtoLogs.toList()
+            .map { it.toLogEntry() }
     }
-
     override fun getAllLogs(): List<LogEntry> {
         TODO("Not yet implemented")
     }
