ce07f9c74a1a25736232bddf41ebf030642de4bd
Talaat <Abdo.talat740@gmil.com>
2025-05-07 21:16:11 +0300

    fix the mongo database dependency injection

--- src/main/kotlin/data/data_source/log_data_source/remote/MongoLogDataSource.kt
+++ src/main/kotlin/data/data_source/log_data_source/remote/MongoLogDataSource.kt
@@ -1,14 +1,14 @@
 package org.qudus.squad.data.data_source.log_data_source.remote
 
-import com.mongodb.kotlin.client.coroutine.MongoCollection
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import org.qudus.squad.data.data_source.log_data_source.LogDataSource
 import org.qudus.squad.model.entity.LogEntry
 
 class MongoLogDataSource(
-    private val logCollection: MongoCollection<LogEntryDto>
+    private val mongoDataBase: MongoDatabase
 ): LogDataSource {
     override suspend fun addNewLog(logEntry: LogEntry) {
-        logCollection.insertOne(logEntry.toLogEntryDto())
+        mongoDataBase.getCollection<LogEntryDto>("logs").insertOne(logEntry.toLogEntryDto())
     }
 
     override fun getLogByTargetId(targetId: String): List<LogEntry> {
