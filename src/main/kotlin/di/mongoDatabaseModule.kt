0fb592f4da2eae49529d834f971bf4ec4f5276ea
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-05-08 01:04:58 +0300

    move utils package to ui package

--- src/main/kotlin/di/mongoDatabaseModule.kt
+++ src/main/kotlin/di/mongoDatabaseModule.kt
@@ -5,14 +5,9 @@ import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import org.koin.dsl.module
 
 
-
-
 val mongoDatabaseModule = module {
     single<MongoClient> {
-        MongoClient.create(
-            "mongodb+srv://abdelrhmanshabana999:abdelrahman12356@planmatecluster.hbbate9.mongodb.net/?retryWrites=true&w=majority&appName=PlanMateCluster"
-        )
-
+        MongoClient.create(System.getenv("MONGO_URI"))
     }
     single<MongoDatabase> {
         get<MongoClient>().getDatabase("plan_mate")
